6635311de4a07c0b0b298147bd0078673f546700
Rob Brennan <rob@therobbrennan.com>
2019-02-10 21:59:27 -0800

    RepositoryIssues now displays RepositoryIssueReactions

--- src/components/RepositoryIssues.js
+++ src/components/RepositoryIssues.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import RepositoryIssueReactions from './RepositoryIssueReactions'
 
 const RepositoryIssues = ({ issues }) => {
   if (!issues) return null
@@ -7,6 +8,7 @@ const RepositoryIssues = ({ issues }) => {
       {issues.map(issue => (
         <li key={issue.node.id}>
           <a href={issue.node.url}>{issue.node.title}</a>
+          <RepositoryIssueReactions reactions={issue.node.reactions.edges} />
         </li>
       ))}
     </ul>
