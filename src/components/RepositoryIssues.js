5d7dd42961477f96495425a4fa434ce0d1fd46ca
Rob Brennan <rob@therobbrennan.com>
2019-02-10 22:30:46 -0800

    Updated RepositoryIssues to conditionally display More button

--- src/components/RepositoryIssues.js
+++ src/components/RepositoryIssues.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import RepositoryIssueReactions from './RepositoryIssueReactions'
 
-const RepositoryIssues = ({ issues }) => {
+const RepositoryIssues = ({ issues, onFetchMoreIssues }) => {
   if (!issues) return null
+  const hasNextPage = issues.pageInfo.hasNextPage
+
   return (
-    <ul>
-      {issues.map(issue => (
-        <li key={issue.node.id}>
-          <a href={issue.node.url}>{issue.node.title}</a>
-          <RepositoryIssueReactions reactions={issue.node.reactions.edges} />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {issues.edges.map(issue => (
+          <li key={issue.node.id}>
+            <a href={issue.node.url}>{issue.node.title}</a>
+            <RepositoryIssueReactions reactions={issue.node.reactions.edges} />
+          </li>
+        ))}
+      </ul>
+      {hasNextPage && <button onClick={onFetchMoreIssues}>More</button>}
+    </div>
   )
 }
 
