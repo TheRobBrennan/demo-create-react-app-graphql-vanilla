0714f93385e4665ef469fa5672b41a73b031eb9f
Rob Brennan <rob@therobbrennan.com>
2019-02-10 22:30:22 -0800

    Updated GitHub lib

--- src/lib/github.js
+++ src/lib/github.js
@@ -11,33 +11,58 @@ export const axiosGitHubGraphQL = axios.create({
 })
 
 // This is a higher order function because we need to pass the result of
-// the promise, and also provide a function for the this.setState method
-// that will be consuming it.
-export const resolveIssuesQuery = queryResult => () => ({
-  organization: queryResult.data.data.organization,
-  errors: queryResult.data.errors,
-})
+// the promise - as well as an optional cursor - and also provide a function for the
+// this.setState method that will be executing it.
+export const resolveIssuesQuery = (queryResult, cursor) => state => {
+  const { data, errors } = queryResult.data
+
+  if (!cursor) {
+    return {
+      organization: data.organization,
+      errors,
+    }
+  }
+
+  // Merge the old data with the new data
+  const { edges: oldIssues } = state.organization.repository.issues
+  const { edges: newIssues } = data.organization.repository.issues
+  const updatedIssues = [...oldIssues, ...newIssues]
+
+  return {
+    organization: {
+      ...data.organization,
+      repository: {
+        ...data.organization.repository,
+        issues: {
+          ...data.organization.repository.issues,
+          edges: updatedIssues,
+        }
+      }
+    },
+    errors,
+  }
+}
 
 // Naive GraphQL query function(s) to return a template literal
-export const getIssuesOfRepository = path => {
+export const getIssuesOfRepository = (path, cursor) => {
   const [organization, repository] = path.split('/')
 
   return axiosGitHubGraphQL.post('', {
     query: GET_ISSUES_OF_REPOSITORY,
-    variables: { organization, repository },
+    variables: { organization, repository, cursor },
   })
 }
 
 // Let's use GraphQL variables to define our template literal
 const GET_ISSUES_OF_REPOSITORY = `
-  query ($organization: String!, $repository: String!) {
+  query ($organization: String!, $repository: String!, $cursor: String) {
     organization (login: $organization) {
       name
       url
       repository (name: $repository) {
         name
         url
-        issues (last:5, states: [OPEN]) {
+        issues (first:5, after:$cursor, states: [OPEN]) {
           edges {
             node {
               id
@@ -52,6 +77,11 @@ const GET_ISSUES_OF_REPOSITORY = `
                 }
               }
             }
+          }
+          totalCount
+          pageInfo {
+            endCursor
+            hasNextPage
           }
         }
       }
