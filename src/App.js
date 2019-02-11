3be4a447bd3ee26f7610bd4a8a79252c9e9f94f3
Rob Brennan <rob@therobbrennan.com>
2019-02-10 19:21:12 -0800

    Initial setup of App with minimal form and event handlers

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
+const TITLE = 'React GraphQL GitHub Client'
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: process.env.REACT_APP_GITHUB_GRAPHQL_ENDPOINT,
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+})
+
+// We are using class field declarations. This allows us the ability to omit the
+// constructor method for initializing local state, and eliminates the need to
+// explicitly bind class methods. Arrow functions will handle all the binding.
 class App extends Component {
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+  }
+
+  componentDidMount() {
+    // Fetch data
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value })
+  }
+
+  onSubmit = event => {
+    // Fetch data
+    event.preventDefault()
+  }
+
   render() {
+    const { path } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{TITLE}</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">Show open issues for https://github.com/</label>
+          <input
+            id="url"
+            type="text"
+            onChange={this.onChange}
+            style={{ width: '300px' }}
+            value={path}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <hr />
+
+        {/* Here comes the result! */}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
